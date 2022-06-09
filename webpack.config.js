561881b21a3542fd261c0eea6256a8e562bdd65e
Harold <cvuyante24@gmail.com>
2022-06-09 14:36:13 +0800

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: 'main.js',
+		filename: './dist/main.js',
 		path: path.resolve(__dirname, './dist')
 	}
 };
