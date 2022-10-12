273f58e979a50f5acc370cd332280bffb451ffce
Harold <cvuyante24@gmail.com>
2022-10-12 14:06:41 +0800

    testing kung dine yun folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
 	const element = document.createElement('div');
 
 	// Lodash, currently included via a script, is required for this line to work
-	element.innerHTML = _.join(['ting', 'wawa'], ' ');
+	element.innerHTML = _.join(['ting', 'GG bruhhhh ano ba'], ' ');
 
 	return element;
 }
